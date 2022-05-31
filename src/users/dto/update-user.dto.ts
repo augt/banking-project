2b133e3686db87bf73ef6cc59d3a3eef9774f5d8
Augustin Tacquet <augu.tacquet@gmail.com>
2022-05-31 17:29:23 +0200

    added validation on user routes

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,10 +1,25 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsAlpha, IsEmail, IsNotEmpty, IsNumber } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
+    @IsNotEmpty()
+    @IsAlpha()
     firstName?: string;
+
+    @IsNotEmpty()
+    @IsAlpha()
     lastName?: string;
+
+    @IsNotEmpty()
+    @IsEmail()
     email?: string;
+
+    @IsNotEmpty()
+    @IsAlpha()
     password?: string;
+
+    @IsNotEmpty()
+    @IsNumber()
     age?: number;
 }
